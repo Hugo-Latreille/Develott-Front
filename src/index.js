9a9f1c6743d6c5f02b97c60da462b6132257da84
Hugo Latreille <hugo.latreille@gmail.com>
2022-07-17 10:37:47 +0200

    🔧 config: utilisation eslint+prettier depuis config create-react-app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Login from "./pages/Login/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root
-	.render
+root.render(
 	// <React.StrictMode>
-	// <App />
+	<Login />
 	// </React.StrictMode>
-	();
+);
