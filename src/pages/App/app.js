5ea9631a57da76b2ff237b83a1336ff3e62312c5
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-07-20 15:37:58 +0200

    :rocket: feat : create route  /projets

--- src/pages/App/app.js
+++ src/pages/App/app.js
@@ -6,12 +6,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Home/home";
 import Login from "../Login/Login";
 import Register from "../Login/Register";
+import Projects from "../Projects/projects";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projets" element={<Projects />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Home />} />
