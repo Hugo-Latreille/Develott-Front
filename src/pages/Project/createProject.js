de6eee60d91e63e14f4c7b2c2cd7c0f3bd61acab
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-07-23 14:32:12 +0200

    :rocket: feat : display each project formss (infos, technos, jobs) on click

--- src/pages/Project/createProject.js
+++ src/pages/Project/createProject.js
@@ -4,13 +4,29 @@ import FooterColored from "./../../components/Footer/footerColored";
 
 import CreateProjectInformationsForm from "./createProjectInformationsForm";
 import CreateProjectTechnologiesForm from "./createProjectTechnologiesForm";
+import CreateProjectJobsForm from "./createProjectJobsForm";
 
 // ajout léa
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function CreateProject() {
   //ajout léa
   const navigate = useNavigate();
+
+  const [activeForm, setActiveForm] = useState("informations");
+
+  const handleDisplayInformationsForm = () => {
+    setActiveForm("informations");
+  };
+
+  const handleDisplayTechnologiesForm = () => {
+    setActiveForm("technologies");
+  };
+
+  const handleDisplayJobsForm = () => {
+    setActiveForm("jobs");
+  };
 
   return (
     <>
@@ -19,20 +35,31 @@ function CreateProject() {
         <div className="create-project-container ">
           <div className="create-project-left">
             <div className="create-project-step">
-              <h2 className="create-project-title">Informations</h2>
+              <h2
+                className="create-project-title"
+                onClick={handleDisplayInformationsForm}
+              >
+                Informations
+              </h2>
               <p className="create-project-desc p-light">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt.
               </p>
             </div>
-            <div className="create-project-step">
+            <div
+              className="create-project-step"
+              onClick={handleDisplayTechnologiesForm}
+            >
               <h2 className="create-project-title">Technologies</h2>
               <p className="create-project-desc p-light">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt.
               </p>
             </div>
-            <div className="create-project-step">
+            <div
+              className="create-project-step"
+              onClick={handleDisplayJobsForm}
+            >
               <h2 className="create-project-title">Profils recherchés</h2>
               <p className="create-project-desc p-light">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -41,7 +68,9 @@ function CreateProject() {
             </div>
           </div>
           <div className="create-project-right">
-            <CreateProjectTechnologiesForm />
+            {activeForm === "informations" && <CreateProjectInformationsForm />}
+            {activeForm === "technologies" && <CreateProjectTechnologiesForm />}
+            {activeForm === "jobs" && <CreateProjectJobsForm />}
           </div>
         </div>
       </div>
--- src/pages/Project/createProjectJobsForm.js
+++ src/pages/Project/createProjectJobsForm.js
@@ -4,19 +4,51 @@ import InputProject from "../../components/Input/ProjectInput";
 import SearchBar from "../../components/SearchBar/searchBar";
 function CreateProjectJobsForm() {
   return (
-    <div>
+    <div className="form-jobs">
       <SearchBar className="navbar-searchbar" />
+      <span className="form-jobs-disclaimer">
+        Veuillez renseigner les profils recherchés pour constituer votre équipe
+        et pour mener à bien votre projet via le champs ci-dessus. Une fois tous
+        les postes renseignés, veuillez passer à l'étape suivante.{" "}
+      </span>
+      <div className="form-jobs-container form-jobs-container-70">
+        <div className="form-jobs-container-content">
+          <h3 className="form-jobs-title">Profils Ajoutés</h3>
+          <div className="form-jobs-items">
+            <p>Développeur Back-end</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+          <div className="form-jobs-items">
+            <p>Développeur Back-end</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+          <div className="form-jobs-items">
+            <p>Développeur Front-end</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+          <div className="form-jobs-items">
+            <p>Scrum master</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+        </div>
+        <div className="form-jobs-container-content">
+          <h3 className="form-jobs-title">Profils Ajoutés</h3>
+          <div className="form-jobs-items">
+            <p>UX-UI Designer</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+          <div className="form-jobs-items">
+            <p>Git Master</p>
+            <i class="fal fa-backspace form-jobs-delete"></i>
+          </div>
+        </div>
+      </div>
       <button
         type="submit"
         className="main-button-colored create-project-button"
       >
         Valider
       </button>
-      <div className="form-jobs-container">
-        <div className="form-jobs-container-content">qd</div>
-        <div className="form-jobs-container-content">qd</div>
-        <div className="form-jobs-container-content">qd</div>
-      </div>
     </div>
   );
 }
