982a627fe428986fbf2c2a2ce64e61903a7b0fcc
Charlelielataste <latastecharlelie@gmail.com>
2022-07-23 17:59:29 +0200

    :art: style: new logo asset for slidbar and :rocket: feat: create folder Charte

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-// import Charte from "./pages/Charte/charte";
 import App from "./pages/App/app";
 import { BrowserRouter } from "react-router-dom";
 
