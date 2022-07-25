7adc454c885c697a211013f5b8e8c1feb1a1e308
Hugo Latreille <hugo.latreille@gmail.com>
2022-07-25 21:43:05 +0200

    🚀feat: login github avec JWT

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -4,14 +4,23 @@ import Cards from "../../components/Cards/cards";
 import FooterColored from "../../components/Footer/footerColored";
 import Sidebar from "../../components/SideBar/sidebar";
 import { useGithubLoginQuery } from "../Login/authAPI";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "../Login/authSlice";
 
 function Projects() {
+	const dispatch = useDispatch();
 	const { data, isSuccess, error } = useGithubLoginQuery({
-		refetchOnMountOrArgChange: true,
+		// refetchOnMountOrArgChange: true,
 	});
 
 	if (isSuccess) {
 		console.log(data);
+		dispatch(
+			setCredentials({
+				accessToken: data.accessToken,
+				email: data.foundUser.email,
+			})
+		);
 	}
 	if (error) {
 		console.log(error);
