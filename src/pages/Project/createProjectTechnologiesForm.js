2acd5d1a0fae61d1c97fc6b3c3ce61588a0b96f2
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-07-25 19:00:53 +0200

    :rocket: feat : display selected technologies on input submit

--- src/pages/Project/createProjectTechnologiesForm.js
+++ src/pages/Project/createProjectTechnologiesForm.js
@@ -2,10 +2,38 @@ import "./createProjectTechnologiesForm.scss";
 
 import InputProject from "../../components/Input/ProjectInput";
 import SearchBarProjects from "./../../components/SearchBar/searchBarTechnologies";
+
+import { useState } from "react";
+
 function CreateProjectTechnologiesForm() {
+  const [technologiesData, setTechnologiesData] = useState([]);
+
+  const handleEditTechnologiesData = (item) => {
+    console.log("coucou");
+    setTechnologiesData([...technologiesData, item]);
+  };
+
+  const languagesData = technologiesData.filter((technology) =>
+    technology.tags.includes("language")
+  );
+
+  const frameworksData = technologiesData.filter((technology) =>
+    technology.tags.includes("framework")
+  );
+
+  const othersData = technologiesData.filter(
+    (technology) =>
+      !technology.tags.includes("framework") &&
+      !technology.tags.includes("language")
+  );
+
   return (
     <div className="form-technologies">
-      <SearchBarProjects className="navbar-searchbar" />
+      <SearchBarProjects
+        technologiesArray={technologiesData}
+        handleTechnologies={handleEditTechnologiesData}
+        className="navbar-searchbar"
+      />
       <span className="form-technologies-disclaimer">
         Veuillez renseigner les technologies souhaitées pour mener à bien votre
         projet via le champs ci-dessus. Une fois toutes les technologies
@@ -14,48 +42,48 @@ function CreateProjectTechnologiesForm() {
       <div className="form-technologies-container form-technologies-container-70">
         <div className="form-technologies-container-content">
           <h3 className="form-technologies-title">Languages</h3>
-          <div className="form-technologies-items">
-            <p>
-              <i className="devicon-javascript-plain colored"></i> Javascript
-            </p>
-            <i className="fal fa-backspace form-technologies-delete"></i>
-          </div>
-          <div className="form-technologies-items">
-            <p>
-              <i className="devicon-html5-plain colored"></i> html5
-            </p>
-            <i className="fal fa-backspace form-technologies-delete"></i>
-          </div>
-          <div className="form-technologies-items">
-            <p>
-              <i className="devicon-css3-plain colored"></i> Css3
-            </p>
-            <i className="fal fa-backspace form-technologies-delete"></i>
-          </div>
-          <div className="form-technologies-items">
-            <p>
-              <i className="devicon-sass-plain colored"></i> sass
-            </p>
-            <i className="fal fa-backspace form-technologies-delete"></i>
-          </div>
+          {languagesData.length === 0 && (
+            <span className="form-technologies-empty">vide...</span>
+          )}
+          {languagesData.map((techno) => (
+            <div className="form-technologies-items">
+              <p>
+                <i className={`devicon-${techno.name}-plain colored`}></i>{" "}
+                {techno.name}
+              </p>
+              <i className="fal fa-backspace form-technologies-delete"></i>
+            </div>
+          ))}
         </div>
         <div className="form-technologies-container-content">
           <h3 className="form-technologies-title">Frameworks</h3>
-          <p>
-            <i className="devicon-react-plain colored"></i> react
-          </p>
-          <p>
-            <i className="devicon-redux-plain colored"></i> redux
-          </p>
-          <p>
-            <i className="devicon-bootstrap-plain colored"></i> bootstrap
-          </p>
+          {frameworksData.length === 0 && (
+            <span className="form-technologies-empty">vide...</span>
+          )}
+          {frameworksData.map((techno) => (
+            <div className="form-technologies-items">
+              <p>
+                <i className={`devicon-${techno.name}-plain colored`}></i>{" "}
+                {techno.name}
+              </p>
+              <i className="fal fa-backspace form-technologies-delete"></i>
+            </div>
+          ))}
         </div>
         <div className="form-technologies-container-content">
           <h3 className="form-technologies-title">Divers</h3>
-          <span className="form-technologies-empty">
-            Vide pour l'instant...
-          </span>
+          {othersData.length === 0 && (
+            <span className="form-technologies-empty">vide...</span>
+          )}
+          {othersData.map((techno) => (
+            <div className="form-technologies-items">
+              <p>
+                <i className={`devicon-${techno.name}-plain colored`}></i>{" "}
+                {techno.name}
+              </p>
+              <i className="fal fa-backspace form-technologies-delete"></i>
+            </div>
+          ))}
         </div>
       </div>
       <button
