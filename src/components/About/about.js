b11798396593ab02b78b36a37587b88de9d87d63
Charlelielataste <latastecharlelie@gmail.com>
2022-07-28 18:01:22 +0200

    :art: style : 3D for About

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,92 +1,77 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./about.scss";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+import Lea from "../../assets/images/Lea.png";
+import PA from "../../assets/images/PA.png";
+import Hugo from "../../assets/images/Hugo.png";
+import Clement from "../../assets/images/Clement.png";
+import Charlelie from "../../assets/images/Charlelie.png";
+
+// import required modules
+import { Autoplay, EffectCube, Pagination } from "swiper";
 
 function About() {
   return (
-    <div className="charte_container">
-      <div className="charte_container_main">
-        <div className="charte_container_title">
-          <h1 className="charte_title">Notre Charte</h1>
+    <div className="about_container">
+      <div className="about_container_main">
+        <div className="about_container_title">
+          <h1 className="about_title">About</h1>
         </div>
-        <div className="charte_main">
-          <section>
-            <p className="charte_main_section">
-              En tant qu’utilisateur de Develott, vous reconnaissez avoir pris
-              connaissance de la présente charte de modération et vous vous
-              engagez à respecter des règles de bonne conduite. Nous nous
-              gardons le droit de supprimer tout Projet qui ne respecterait pas
-              les règles de la présente charte d’utilisation. Vous vous engagez
-              notamment à respecter les lois et règlements en vigueur ainsi que
-              le droit des personnes. Sont notamment illicites (liste non
-              exhaustive) :
-            </p>
-            <ul>
-              <li className="charte_main_list">
-                l’incitation à la haine raciale, au révisionnisme ou au
-                négationnisme
-              </li>{" "}
-              <li className="charte_main_list">
-                l’insulte (injures, propos à caractère sexistes, homophobes,
-                grossiers, agressifs, irrévérencieux…)
-              </li>{" "}
-              <li className="charte_main_list">
-                la diffamation (imputation d’un fait portant atteinte à
-                l’honneur ou à la considération de la personne physique ou
-                morale, ou du corps auquel le fait est imputé)
-              </li>{" "}
-              <li className="charte_main_list">
-                les propos discriminatoires, sous toutes les formes
-              </li>{" "}
-              <li className="charte_main_list">
-                l’incitation à la consommation de drogues, d’alcool ou de tabac
-              </li>{" "}
-              <li className="charte_main_list">
-                les copies de messages privés ou de correspondance (violation du
-                secret de la correspondance)
-              </li>{" "}
-              <li className="charte_main_list">la pédophilie</li>{" "}
-              <li className="charte_main_list">
-                les citations d’auteurs (extraits de livres, sites…). Toutefois,
-                les analyses et courtes citations sont possibles, sous réserve
-                que soient indiqués clairement le nom de l’auteur et la source.
-                Plus généralement le contributeur s’engage à respecter les
-                droits de propriété intellectuelle des auteurs
-              </li>{" "}
-              <li className="charte_main_list">
-                les contributions ayant la nature de publicités. Plus
-                généralement, tout utilisateur s’engage à respecter des règles
-                de bonnes conduite et à : ne pas écrire entièrement en langage
-                abrégé ou en majuscules
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas publier des messages en plusieurs exemplaires ; ne pas
-                mettre en cause des personnes particulières
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas publier des petites annonces
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas publier des commentaires à caractère publicitaire
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas publier des commentaires hors sujet (par exemple une
-                demande de renseignement sur un dossier administratif personnel,
-                etc..)
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas contribuer en associant un lien hypertexte renvoyant vers
-                un site commercial
-              </li>{" "}
-              <li className="charte_main_list">
-                ne pas utiliser le site de manière abusive ou malhonnête,
-                notamment, sans que cela soit limitatif, en multipliant les
-                messages dans le but d’entraver ou de fausser le fonctionnement
-                de cet espace (phénomènes pollupostage)
-              </li>{" "}
-            </ul>
-            <p className="charte_main_section">Merci Bisous</p>
-          </section>
+        <div className="about_main">
+          <Swiper
+            effect={"cube"}
+            grabCursor={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+            cubeEffect={{
+              shadow: false,
+              slideShadows: true,
+              shadowOffset: 20,
+              shadowScale: 2,
+            }}
+            pagination={true}
+            modules={[Autoplay, EffectCube, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={Lea} />
+              <p className="cube_name">Léa Bramoullé</p>
+              <p className="cube_name">Product Owner / Dev Front</p>
+              <div class="overlay">
+                <div class="text">
+                  <i class="fas fa-arrow-alt-left swipe_icon"></i>Swipe
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Clement} />
+              <p className="cube_name">Clément Moretti</p>
+              <p className="cube_name">Lead Dev Back</p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Hugo} />
+              <p className="cube_name">Hugo Latreille</p>
+              <p className="cube_name">Lead Dev Front</p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={PA} />
+              <p className="cube_name">Pierre-Antoine Potage</p>
+              <p className="cube_name">Git Master / Dev Back</p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Charlelie} />
+              <p className="cube_name">Charlelie Lataste</p>
+              <p className="cube_name">Scrum Master / Dev Front</p>
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <div className="charte_container_end"></div>
+        <div className="about_container_end"></div>
       </div>
     </div>
   );
--- src/components/SideBar/sidebar.js
+++ src/components/SideBar/sidebar.js
@@ -15,223 +15,220 @@ import { logOut } from "../../pages/Login/authSlice";
 import { closeSideBar, toggleSideBar } from "../../pages/App/appSlice";
 
 const routes = [
-
-	{
-		path: "/",
-		name: "Dashboard",
-		icon: <AiOutlineDashboard />,
-	},
-	{
-		path: "/",
-		name: "Message",
-		icon: <MdMessage />,
-	},
-	{
-		path: "/projets",
-		name: "Projets",
-		icon: <FaProjectDiagram />,
-	},
-	{
-		path: "/projet/create",
-		name: "Créer Projet",
-		icon: <MdConstruction />,
-	},
+  {
+    path: "/",
+    name: "Dashboard",
+    icon: <AiOutlineDashboard />,
+  },
+  {
+    path: "/",
+    name: "Message",
+    icon: <MdMessage />,
+  },
+  {
+    path: "/projets",
+    name: "Projets",
+    icon: <FaProjectDiagram />,
+  },
+  {
+    path: "/projet/create",
+    name: "Créer Projet",
+    icon: <MdConstruction />,
+  },
 ];
 
 function Sidebar({ children, isVisible }) {
-	const [darkMode, setDarkMode] = useState(false);
-	const isOpen = useSelector((state) => state.app.sideBarIsOpen);
-	const dispatch = useDispatch();
-	const [userLogout] = useUserLogoutMutation();
-
+  const [darkMode, setDarkMode] = useState(false);
+  const isOpen = useSelector((state) => state.app.sideBarIsOpen);
+  const dispatch = useDispatch();
+  const [userLogout] = useUserLogoutMutation();
 
   const handleLogout = async () => {
     await userLogout();
     dispatch(logOut());
   };
 
-<<<<<<< HEAD
-	const showAnimation = {
-		hidden: {
-			width: 0,
-			opacity: 0,
-			transition: {
-				duration: 0.3,
-			},
-		},
-		show: {
-			width: "auto",
-			opacity: 1,
-			transition: {
-				duration: 0.3,
-			},
-		},
-	};
-	const logoAnimation = {
-		hidden: {
-			width: "1em",
-			opacity: 0,
-			transition: {
-				duration: 0.5,
-			},
-		},
-		show: {
-			width: "2rem",
-			opacity: 1,
-			transition: {
-				duration: 0.5,
-			},
-		},
-	};
-	const logAnimation = {
-		hidden: {
-			width: 0,
-			opacity: 0,
-			transition: {
-				duration: 0.1,
-			},
-		},
-		show: {
-			width: "auto",
-			rotate: 360,
-			scale: 1,
-			opacity: 1,
-			transition: {
-				duration: 0.4,
-			},
-		},
-	};
-	const profilAnimation = {
-		hidden: {
-			width: 0,
-			opacity: 0,
-			transition: {
-				duration: 0.5,
-			},
-		},
-		show: {
-			width: "auto",
-			rotate: 360,
-			opacity: 1,
-			transition: {
-				duration: 0.4,
-			},
-		},
-	};
-	return (
-		<div className="sidebar_container">
-			<motion.div
-				initial={{ width: isOpen ? "250px" : "54px" }}
-				animate={{
-					width: isOpen ? "250px" : "54px",
-					transition: {
-						duration: 0.5,
-						type: "spring",
-						damping: 12,
-					},
-				}}
-				className="sidebar"
-			>
-				<div className="top_section">
-					{isOpen && (
-						<NavLink to="/">
-							<AnimatePresence initial={false}>
-								<motion.h1
-									key="top_section"
-									initial="hidden"
-									animate="show"
-									exit="hidden"
-									variants={showAnimation}
-									className="logo_nav"
-								>
-									Develott
-								</motion.h1>
-							</AnimatePresence>
-						</NavLink>
-					)}
-					<div className="bars">
-						{isOpen && (
-							<AnimatePresence initial={false}>
-								<motion.img
-									key="bars"
-									whileHover={{ scale: 1.5, rotate: 180 }}
-									whileTap={{
-										scale: 0.8,
-										rotate: -90,
-										borderRadius: "100%",
-									}}
-									initial="hidden"
-									animate="show"
-									exit="hidden"
-									variants={logoAnimation}
-									className="bars_logo"
-									src={LogoW}
-									alt="logo"
-									onClick={() => dispatch(toggleSideBar())}
-								/>
-							</AnimatePresence>
-						)}
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    show: {
+      width: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+  const logoAnimation = {
+    hidden: {
+      width: "1em",
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      width: "2rem",
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+  const logAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.1,
+      },
+    },
+    show: {
+      width: "auto",
+      rotate: 360,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+  const profilAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      width: "auto",
+      rotate: 360,
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+  return (
+    <div className="sidebar_container">
+      <motion.div
+        initial={{ width: isOpen ? "250px" : "54px" }}
+        animate={{
+          width: isOpen ? "250px" : "54px",
+          transition: {
+            duration: 0.5,
+            type: "spring",
+            damping: 12,
+          },
+        }}
+        className="sidebar"
+      >
+        <div className="top_section">
+          {isOpen && (
+            <NavLink to="/">
+              <AnimatePresence initial={false}>
+                <motion.h1
+                  key="top_section"
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={showAnimation}
+                  className="logo_nav"
+                >
+                  Develott
+                </motion.h1>
+              </AnimatePresence>
+            </NavLink>
+          )}
+          <div className="bars">
+            {isOpen && (
+              <AnimatePresence initial={false}>
+                <motion.img
+                  key="bars"
+                  whileHover={{ scale: 1.5, rotate: 180 }}
+                  whileTap={{
+                    scale: 0.8,
+                    rotate: -90,
+                    borderRadius: "100%",
+                  }}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={logoAnimation}
+                  className="bars_logo"
+                  src={LogoW}
+                  alt="logo"
+                  onClick={() => dispatch(toggleSideBar())}
+                />
+              </AnimatePresence>
+            )}
 
-						<AnimatePresence initial={false}>
-							{!isOpen && (
-								<motion.img
-									key="bars_closed"
-									whileHover={{ scale: 1.4, rotate: 180 }}
-									whileTap={{
-										scale: 0.8,
-										rotate: -90,
-										borderRadius: "100%",
-									}}
-									initial="hidden"
-									animate="show"
-									exit="hidden"
-									variants={logoAnimation}
-									className="bars_logo"
-									src={LogoW}
-									alt="logo"
-									onClick={() => dispatch(toggleSideBar())}
-								/>
-							)}
-						</AnimatePresence>
-					</div>
-
-					{isOpen && <div className="anim"></div>}
-				</div>
-				<div className="space" onClick={() => dispatch(toggleSideBar())}></div>
-				<section className="sidebar_icon">
-					{routes.map((route) => (
-						<NavLink
-							to={route.path}
-							key={route.name}
-							className="sidebar_navlink"
-						>
-							<div className="side_icon">{route.icon}</div>
-							<AnimatePresence initial={false}>
-								{isOpen && (
-									<motion.div
-										key={route.name}
-										initial="hidden"
-										animate="show"
-										exit="hidden"
-										variants={showAnimation}
-										className="side_text"
-									>
-										{route.name}
-									</motion.div>
-								)}
-							</AnimatePresence>
-						</NavLink>
-					))}
-					<div
-						onClick={() => dispatch(toggleSideBar())}
-						className="active_toggle"
-					></div>
-					<div className="bottom_container">
-					<div className="toggle_pic_container">
-						<AnimatePresence initial={false}>
+            <AnimatePresence initial={false}>
               {!isOpen && (
+                <motion.img
+                  key="bars_closed"
+                  whileHover={{ scale: 1.4, rotate: 180 }}
+                  whileTap={{
+                    scale: 0.8,
+                    rotate: -90,
+                    borderRadius: "100%",
+                  }}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={logoAnimation}
+                  className="bars_logo"
+                  src={LogoW}
+                  alt="logo"
+                  onClick={() => dispatch(toggleSideBar())}
+                />
+              )}
+            </AnimatePresence>
+          </div>
+
+          {isOpen && <div className="anim"></div>}
+        </div>
+        <div className="space" onClick={() => dispatch(toggleSideBar())}></div>
+        <section className="sidebar_icon">
+          {routes.map((route) => (
+            <NavLink
+              to={route.path}
+              key={route.name}
+              className="sidebar_navlink"
+            >
+              <div className="side_icon">{route.icon}</div>
+              <AnimatePresence initial={false}>
+                {isOpen && (
+                  <motion.div
+                    key={route.name}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    variants={showAnimation}
+                    className="side_text"
+                  >
+                    {route.name}
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </NavLink>
+          ))}
+          <div
+            onClick={() => dispatch(toggleSideBar())}
+            className="active_toggle"
+          ></div>
+          <div className="bottom_container">
+            <div className="toggle_pic_container">
+              <AnimatePresence initial={false}>
+                {!isOpen && (
                   <motion.i
-									key="toggle_pic_container_closed"
+                    key="toggle_pic_container_closed"
                     initial="hidden"
                     animate="show"
                     exit="hidden"
@@ -246,13 +243,13 @@ function Sidebar({ children, isVisible }) {
                       darkMode ? "fas fa-moon moon" : "fas fa-sun sun"
                     }`}
                   ></motion.i>
-									)}
-									</AnimatePresence>
-									<AnimatePresence initial={false}>
-              {!isOpen && (
+                )}
+              </AnimatePresence>
+              <AnimatePresence initial={false}>
+                {!isOpen && (
                   <NavLink to="/">
                     <motion.img
-										key='toggle_pic_container'
+                      key="toggle_pic_container"
                       initial="hidden"
                       animate="show"
                       exit="hidden"
@@ -267,12 +264,11 @@ function Sidebar({ children, isVisible }) {
                       className="profile_img_close"
                     ></motion.img>
                   </NavLink>
-              )}
-							</AnimatePresence>
+                )}
+              </AnimatePresence>
             </div>
 
-
-						{/* {!isOpen && (
+            {/* {!isOpen && (
 							<AnimatePresence initial={false}>
 								<motion.i
 									key="profile_container_closed"
@@ -312,76 +308,75 @@ function Sidebar({ children, isVisible }) {
 								</motion.div>
 							</AnimatePresence>
 						)} */}
-						<div className="profile">
-							<AnimatePresence initial={false}>
-								{!isOpen && (
-									<motion.div
-										key="profile_closed"
-										initial="hidden"
-										animate="show"
-										exit="hidden"
-										transition={{
-											type: "spring",
-											stiffness: 260,
-											damping: 20,
-										}}
-										variants={logAnimation}
-									>
-										<NavLink to="/" className="logo_unlog">
-											<MdOutlineLogout onClick={handleLogout} />
-										</NavLink>
-									</motion.div>
-								)}
-							</AnimatePresence>
-							<AnimatePresence initial={false}>
-								{isOpen && (
-									<motion.div
-										key="profile"
-										initial="hidden"
-										animate="show"
-										exit="hidden"
-										transition={{
-											type: "spring",
-											stiffness: 260,
-											damping: 20,
-										}}
-										variants={profilAnimation}
-										className="profile_details"
-									>
-										<img
-											className="profile_img"
-											src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile.png"
-											alt="Profil"
-										/>
-										<div className="name_job">
-											<div className="name">Bruce Wayne</div>
-											<div className="job">Lead Dev Front</div>
-										</div>
-									</motion.div>
-								)}
-								{isOpen && (
-									<NavLink
-										to="/"
-										className="logo_unlog_big"
-										onClick={handleLogout}
-									>
-										<MdOutlineLogout />
-									</NavLink>
-								)}
-							</AnimatePresence>
-						</div>
-					</div>
-				</section>
-			</motion.div>
-			<main
-				className="side_main"
-				// onClick={() => dispatch(closeSideBar())}
-			>
-				{children}
-			</main>
-		</div>
-	);
-
+            <div className="profile">
+              <AnimatePresence initial={false}>
+                {!isOpen && (
+                  <motion.div
+                    key="profile_closed"
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                    }}
+                    variants={logAnimation}
+                  >
+                    <NavLink to="/" className="logo_unlog">
+                      <MdOutlineLogout onClick={handleLogout} />
+                    </NavLink>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+              <AnimatePresence initial={false}>
+                {isOpen && (
+                  <motion.div
+                    key="profile"
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                    }}
+                    variants={profilAnimation}
+                    className="profile_details"
+                  >
+                    <img
+                      className="profile_img"
+                      src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile.png"
+                      alt="Profil"
+                    />
+                    <div className="name_job">
+                      <div className="name">Bruce Wayne</div>
+                      <div className="job">Lead Dev Front</div>
+                    </div>
+                  </motion.div>
+                )}
+                {isOpen && (
+                  <NavLink
+                    to="/"
+                    className="logo_unlog_big"
+                    onClick={handleLogout}
+                  >
+                    <MdOutlineLogout />
+                  </NavLink>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
+        </section>
+      </motion.div>
+      <main
+        className="side_main"
+        // onClick={() => dispatch(closeSideBar())}
+      >
+        {children}
+      </main>
+    </div>
+  );
 }
 
 export default Sidebar;
