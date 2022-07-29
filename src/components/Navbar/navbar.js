aa9e2646f99e07f11647d22fdd89c43f5c67b4ce
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-07-29 11:24:47 +0200

    :bug: fix : fix css & responsive landing page + connexion form

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -7,39 +7,39 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleLoggingModalOpen } from "../../pages/Login/authSlice";
 
 function Navbar() {
-	const location = useLocation();
-	const dispatch = useDispatch();
-	const { token } = useSelector((state) => state.auth);
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
 
-	return (
-		<div className="navbar">
-			<Link to="/">
-				<img
-					className="navbar-logo"
-					src={require(`./../../assets/images/v3-large-white.png`)}
-					alt="logo Develott"
-				/>
-			</Link>
-			{/* <ToggleDark /> */}
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img
+          className="navbar-logo"
+          src={require(`./../../assets/images/v3-large-white.png`)}
+          alt="logo Develott"
+        />
+      </Link>
+      {/* <ToggleDark /> */}
 
-			<SearchBar className="navbar-searchbar" />
-			<div className="navbar-link">
-				{/* TO DO : AJOUTER LIEN DARK/LIGHT MODE */}
-				{!token && (
-					<Link
-						to="/connexion"
-						className="main-button-white"
-						state={{ background: location }}
-						// onClick={toggleModal}
-						onClick={() => dispatch(toggleLoggingModalOpen())}
-					>
-						Se connecter
-					</Link>
-				)}
-			</div>
-			<Outlet />
-		</div>
-	);
+      {/* <SearchBar className="navbar-searchbar" /> */}
+      <div className="navbar-link">
+        {/* TO DO : AJOUTER LIEN DARK/LIGHT MODE */}
+        {!token && (
+          <Link
+            to="/connexion"
+            className="main-button-white"
+            state={{ background: location }}
+            // onClick={toggleModal}
+            onClick={() => dispatch(toggleLoggingModalOpen())}
+          >
+            Se connecter
+          </Link>
+        )}
+      </div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Navbar;
