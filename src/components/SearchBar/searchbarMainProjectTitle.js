41b7eef0c18da4afa821eb2d55201354635a2a4a
Hugo Latreille <hugo.latreille@gmail.com>
2022-08-04 23:55:42 +0200

    feat: update title searchbar settings

--- src/components/SearchBar/searchbarMainProjectTitle.js
+++ src/components/SearchBar/searchbarMainProjectTitle.js
@@ -7,46 +7,48 @@ import { setSearchProjectName } from "./searchbarSlice";
 import { useGetAllProjectsQuery } from "./../../pages/Projects/projectsAPISlice";
 
 function SearchbarMainProjectTitle() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { data: projectsTeams } = useGetAllProjectsQuery();
-  let allProjects = projectsTeams?.projects;
-  console.log(allProjects);
+	const { data: projectsTeams } = useGetAllProjectsQuery();
+	let allProjects = projectsTeams?.projects;
+	console.log(allProjects);
 
-  const handleOnSelect = (item) => {
-    // the item selected
-    console.log(item);
-    // dispatch(setSearchProjectName(item.id));
-  };
+	const handleOnSelect = (item) => {
+		// the item selected
+		console.log(item);
+		// dispatch(setSearchProjectName(item.id));
+	};
 
-  const formatResult = (item) => {
-    return (
-      <div className="results-container">
-        <span className="result-span-title">{item.project}</span>
-      </div>
-    );
-  };
+	const formatResult = (item) => {
+		return (
+			<div className="results-container">
+				<span className="result-span-title">{item.project}</span>
+			</div>
+		);
+	};
 
-  return (
-    <div className="projects-searcbar-main-title">
-      <ReactSearchAutocomplete
-        items={projectsTeams?.projects}
-        onSelect={handleOnSelect}
-        styling={{
-          zIndex: 1,
-          backgroundColor: "white",
-          fontSize: "0.8rem",
-        }}
-        formatResult={formatResult}
-        maxResults={5}
-        showIcon={true}
-        placeholder="Nom de projet... "
-        // showNoResults={false}
-        // showNoResultsText="Pas de résultats."
-        showItemsOnFocus={true}
-      />
-    </div>
-  );
+	return (
+		<div className="projects-searcbar-main-title">
+			<ReactSearchAutocomplete
+				items={projectsTeams?.projects}
+				fuseOptions={{ keys: ["project"] }}
+				resultStringKeyName="project"
+				onSelect={handleOnSelect}
+				styling={{
+					zIndex: 1,
+					backgroundColor: "white",
+					fontSize: "0.8rem",
+				}}
+				formatResult={formatResult}
+				maxResults={5}
+				showIcon={true}
+				placeholder="Nom de projet... "
+				// showNoResults={false}
+				// showNoResultsText="Pas de résultats."
+				// showItemsOnFocus={true}
+			/>
+		</div>
+	);
 }
 
 export default SearchbarMainProjectTitle;
