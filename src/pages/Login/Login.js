27781eca0bfc038e42770c804492b4ebf75475f5
Hugo Latreille <hugo.latreille@gmail.com>
2022-08-07 10:02:55 +0200

    :bug:debug: clear inputs register

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,53 +7,54 @@ import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
 import { togglePasswordVisibility } from "./authSlice";
 
 function Login({ onSubmit }) {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const github = () => {
-    window.open("https://develott.herokuapp.com/v1/auth/github", "_self");
-  };
+	const github = () => {
+		// window.open("https://develott.herokuapp.com/v1/auth/github", "_self");
+		window.open("http://localhost:3002/v1/auth/github", "_self");
+	};
 
-  const {
-    email,
-    password,
-    passwordVisibility,
-    passwordFocus,
-    passwordValidity,
-  } = useSelector((state) => state.auth);
+	const {
+		email,
+		password,
+		passwordVisibility,
+		passwordFocus,
+		passwordValidity,
+	} = useSelector((state) => state.auth);
 
-  return (
-    <div className="login-container">
-      {/* <h2>Se connecter</h2> */}
-      <form className="login-form" onSubmit={onSubmit}>
-        <div className="login-form-inputs">
-          <div className="form">
-            <Input
-              name="email"
-              value={email}
-              label="Email"
-              type="email"
-              required={true}
-            />
-            <Input
-              name="password"
-              value={password}
-              label="Mot de passe"
-              type={passwordVisibility ? "text" : "password"}
-              required={true}
-            />
-            <div
-              className="toogle_eye"
-              onClick={() => dispatch(togglePasswordVisibility())}
-            >
-              {passwordVisibility ? (
-                <MdOutlineVisibility />
-              ) : (
-                <MdOutlineVisibilityOff />
-              )}
-            </div>
-          </div>
+	return (
+		<div className="login-container">
+			{/* <h2>Se connecter</h2> */}
+			<form className="login-form" onSubmit={onSubmit}>
+				<div className="login-form-inputs">
+					<div className="form">
+						<Input
+							name="email"
+							value={email}
+							label="Email"
+							type="email"
+							required={true}
+						/>
+						<Input
+							name="password"
+							value={password}
+							label="Mot de passe"
+							type={passwordVisibility ? "text" : "password"}
+							required={true}
+						/>
+						<div
+							className="toogle_eye"
+							onClick={() => dispatch(togglePasswordVisibility())}
+						>
+							{passwordVisibility ? (
+								<MdOutlineVisibility />
+							) : (
+								<MdOutlineVisibilityOff />
+							)}
+						</div>
+					</div>
 
-          {/* {passwordFocus && (
+					{/* {passwordFocus && (
 						<div>
 							Le mot de passe doit contenir :
 							<ul>
@@ -72,23 +73,23 @@ function Login({ onSubmit }) {
 							</ul>
 						</div>
 					)} */}
-        </div>
-        <div className="login-form-buttons">
-          <button type="submit" className="main-button-colored">
-            Se connecter
-          </button>
-          <a href="#" className="secondary-button-colored" onClick={github}>
-            <i className="fab fa-github"></i> Github
-          </a>
-          <Link
-            to={"../forgotpassword"}
-            className="third-button-colored width-100"
-          >
-            Mot de passe oublié ?
-          </Link>
-        </div>
-      </form>
-    </div>
-  );
+				</div>
+				<div className="login-form-buttons">
+					<button type="submit" className="main-button-colored">
+						Se connecter
+					</button>
+					<a href="#" className="secondary-button-colored" onClick={github}>
+						<i className="fab fa-github"></i> Github
+					</a>
+					<Link
+						to={"../forgotpassword"}
+						className="third-button-colored width-100"
+					>
+						Mot de passe oublié ?
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
 }
 export default Login;
--- src/pages/Login/authSlice.js
+++ src/pages/Login/authSlice.js
@@ -1,27 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userId: "",
-  firstname: "",
-  lastname: "",
-  password: "",
-  passwordConfirm: "",
-  email: "",
-  username_gith: "",
-  isLogged: false,
-  isLoggingActive: true,
-  token: "",
-  loggingModalOpen: false,
-  passwordVisibility: false,
-  registerPasswordVisibility: false,
-  passwordValidity: {
-    minChar: null,
-    number: null,
-    uppercase: null,
-    specialChar: null,
-  },
-  passwordFocus: false,
-  passwordValidationWidth: 0,
+	userId: "",
+	firstname: "",
+	lastname: "",
+	password: "",
+	passwordConfirm: "",
+	email: "",
+	username_gith: "",
+	isLogged: false,
+	isLoggingActive: true,
+	token: "",
+	loggingModalOpen: false,
+	passwordVisibility: false,
+	registerPasswordVisibility: false,
+	passwordValidity: {
+		minChar: null,
+		number: null,
+		uppercase: null,
+		specialChar: null,
+	},
+	passwordFocus: false,
+	passwordValidationWidth: 0,
 };
 
 const isNumberRegex = /\d/;
@@ -29,68 +29,69 @@ const specialCharacterRegex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 const oneUppercase = /[A-Z]/;
 
 export const authSlice = createSlice({
-  name: "login",
-  initialState,
-  reducers: {
-    handleChange: (state, action) => {
-      state[action.payload.name] = action.payload.value;
-    },
-    clearInputs: (state) => {
-      state.firstname = "";
-      state.lastname = "";
-      state.password = "";
-      state.passwordConfirm = "";
-      state.email = "";
-    },
-    toggleLoggingActive: (state) => {
-      state.isLoggingActive = !state.isLoggingActive;
-    },
-    setCredentials: (state, action) => {
-      const { email, accessToken } = action.payload;
-      state.email = email;
-      state.token = accessToken;
-      state.isLogged = true;
-    },
-    logOut: (state) => {
-      state.email = "";
-      state.token = "";
-      state.isLogged = false;
-    },
-    toggleLoggingModalOpen: (state) => {
-      state.loggingModalOpen = !state.loggingModalOpen;
-    },
-    togglePasswordVisibility: (state) => {
-      state.passwordVisibility = !state.passwordVisibility;
-    },
-    toggleRegisterVisibility: (state) => {
-      state.registerPasswordVisibility = !state.registerPasswordVisibility;
-    },
-    setPasswordValidity: (state) => {
-      state.passwordValidity = {
-        minChar: state.password.length >= 8 ? true : false,
-        number: isNumberRegex.test(state.password) ? true : false,
-        uppercase: oneUppercase.test(state.password) ? true : false,
-        specialChar: specialCharacterRegex.test(state.password) ? true : false,
-      };
-    },
-    setPasswordValidationWidth: (state, action) => {
-      state.passwordValidationWidth = Number(action.payload);
-    },
-  },
+	name: "login",
+	initialState,
+	reducers: {
+		handleChange: (state, action) => {
+			state[action.payload.name] = action.payload.value;
+		},
+		clearInputs: (state) => {
+			state.firstname = "";
+			state.lastname = "";
+			state.password = "";
+			state.passwordConfirm = "";
+			state.email = "";
+			state.username_gith = "";
+		},
+		toggleLoggingActive: (state) => {
+			state.isLoggingActive = !state.isLoggingActive;
+		},
+		setCredentials: (state, action) => {
+			const { email, accessToken } = action.payload;
+			state.email = email;
+			state.token = accessToken;
+			state.isLogged = true;
+		},
+		logOut: (state) => {
+			state.email = "";
+			state.token = "";
+			state.isLogged = false;
+		},
+		toggleLoggingModalOpen: (state) => {
+			state.loggingModalOpen = !state.loggingModalOpen;
+		},
+		togglePasswordVisibility: (state) => {
+			state.passwordVisibility = !state.passwordVisibility;
+		},
+		toggleRegisterVisibility: (state) => {
+			state.registerPasswordVisibility = !state.registerPasswordVisibility;
+		},
+		setPasswordValidity: (state) => {
+			state.passwordValidity = {
+				minChar: state.password.length >= 8 ? true : false,
+				number: isNumberRegex.test(state.password) ? true : false,
+				uppercase: oneUppercase.test(state.password) ? true : false,
+				specialChar: specialCharacterRegex.test(state.password) ? true : false,
+			};
+		},
+		setPasswordValidationWidth: (state, action) => {
+			state.passwordValidationWidth = Number(action.payload);
+		},
+	},
 });
 
 export const {
-  handleChange,
-  toggleLoggingActive,
-  clearInputs,
-  handleTest,
-  setCredentials,
-  logOut,
-  toggleLoggingModalOpen,
-  togglePasswordVisibility,
-  toggleRegisterVisibility,
-  setPasswordValidity,
-  setPasswordValidationWidth,
+	handleChange,
+	toggleLoggingActive,
+	clearInputs,
+	handleTest,
+	setCredentials,
+	logOut,
+	toggleLoggingModalOpen,
+	togglePasswordVisibility,
+	toggleRegisterVisibility,
+	setPasswordValidity,
+	setPasswordValidationWidth,
 } = authSlice.actions;
 
 export default authSlice.reducer;
