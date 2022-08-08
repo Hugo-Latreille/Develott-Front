31dbd3bf7fd96892caf406cd94b809bae232f114
Hugo Latreille <hugo.latreille@gmail.com>
2022-08-08 08:01:52 +0200

    :bug:hotfix remove console error composant nesting

--- src/components/Profil/profil.js
+++ src/components/Profil/profil.js
@@ -354,19 +354,18 @@ function Profil() {
 											</p>
 											<p className="desc_container_title">
 												<i className="fab fa-linkedin color-secondary"></i>
-												<a href="#">
-													{user?.url_linkedin ? (
-														<a
-															href={`${user?.url_linkedin}`}
-															target="_blank"
-															rel="noreferrer"
-														>
-															{user?.url_linkedin}
-														</a>
-													) : (
-														"Linkedin"
-													)}
-												</a>
+
+												{user?.url_linkedin ? (
+													<a
+														href={`${user?.url_linkedin}`}
+														target="_blank"
+														rel="noreferrer"
+													>
+														{user?.url_linkedin}
+													</a>
+												) : (
+													"Linkedin"
+												)}
 											</p>
 											<p className="desc_container_title">
 												<i className="fas fa-globe color-secondary"></i>
