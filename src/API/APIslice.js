4024cd309dc25a9d4b8f62492e6292926c0ffac0
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-29 16:11:18 +0100

    deploy o2switch init

--- src/API/APIslice.js
+++ src/API/APIslice.js
@@ -3,7 +3,8 @@ import { logOut, setCredentials } from "../pages/Login/authSlice";
 
 const baseQuery = fetchBaseQuery({
 	// baseUrl: "https://develott.herokuapp.com/v1/",
-	baseUrl: "http://localhost:3002/v1/",
+	// baseUrl: "http://localhost:3002/v1/",
+	baseUrl: "https://api.develott.fr/v1/",
 
 	credentials: "include",
 	tagTypes: ["Project", "User"],
